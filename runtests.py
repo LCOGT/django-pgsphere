61ed92640cce320fc9c8bac6b68fdd429277ce29
Austin Riba <ariba@lcogt.net>
2015-11-17 16:05:05 -0800

    add tox, remove interactivity

--- runtests.py
+++ runtests.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
-    test_runner = TestRunner(verbosity=2)
+    test_runner = TestRunner(verbosity=2, interactive=False, keepdb=False)
     failures = test_runner.run_tests(['tests'])
     sys.exit(bool(failures))
